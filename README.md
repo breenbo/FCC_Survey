cf05210515c2ad568d5e98bb14a4624f46fbf1c6
breenbo <berrehuelbruno@gmail.com>
2018-08-11 21:58:58 -0400

    final version

---
+++ README.md
@@ -0,0 +1,15 @@
+# FCC project - Survey
+## What ?
+The second project of the [freecodecamp curriculum](https://www.freecodecamp.org/breenbo) : build a survey form.\\
+The backgrounds change depending of the users choices.
+
+
+## How ?
+Fully responsive : 3 templates depending of the device width.\\
+No framewoks. Use HTML, CSS, plain Javascript.
+
+Use of : 
+- css variables
+- svg pictures
+- css grid system
+- hsl color system
--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-var dropdown = document.getElementById('dropdown')
-dropdown.addEventListener('click', function() {
-  var animal = dropdown.value
-  var header = document.getElementsByTagName('header')[0]
+function changeBackground (el, value) {
+  var element = document.getElementById(el)
   // remove former class and add the new one chosen by user
-  var formerClass = header.className
-  if (formerClass != "") {
-    header.classList.remove(formerClass)
+  var formerClass = element.className
+  if (formerClass !== '') {
+    element.classList.remove(formerClass)
   }
-  header.classList.add(animal)
+  element.classList.add(value)
+}
+
+var dropdown = document.getElementById('dropdown')
+dropdown.addEventListener('click', function () {
+  var animal = dropdown.value
+  // remove former class and add the new one chosen by user
+  changeBackground('header', animal)
 })
+
+var save = document.getElementById('field_save')
+save.addEventListener('click', function () {
+  var saveValue = document.querySelector('input[name=save]:checked').value
+  // change the background depending on the user choice
+  changeBackground('general_background', saveValue)
+})
+
+// set change colors of logos on hover
+var logos = document.getElementsByTagName('svg')
+for (const logo of logos) {
+  let logoName = logo.id
+  let backName = logoName + '_back'
+  let drawName = logoName + '_draw'
+  logo.addEventListener('mouseenter', function () {
+    logo.children[backName].setAttribute('fill', 'hsl(260,100%,60%)')
+    logo.children[drawName].setAttribute('fill', 'hsl(260,50%,90%)')
+  })
+  logo.addEventListener('mouseleave', function () {
+    logo.children[backName].setAttribute('fill', 'hsl(260,50%,90%)')
+    logo.children[drawName].setAttribute('fill', 'hsl(260,100%,60%)')
+  })
+}
